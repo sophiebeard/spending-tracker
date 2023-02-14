d034e7fc60241df470cecc34bdf1a8c9debfdffc
Sophie Beard <88347024+sophiebeard@users.noreply.github.com>
2023-02-14 11:57:56 +0000

    Conditional navigation

--- client/src/index.js
+++ client/src/index.js
@@ -2,35 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
+  RouterProvider
 } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Register from './pages/Register';
+import router from './routes.js';
 
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-    path: '/login',
-    element: <Login />,
-      },
-      {
-    path: '/register',
-    element: <Register />,
-      },
-    ]
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ client/src/routes.js
@@ -0,0 +1,39 @@
+import App from './App';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import { createBrowserRouter } from 'react-router-dom';
+import CheckAuth from './utils/CheckAuth';
+import Guest from './utils/Guest';
+
+export default createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <CheckAuth>
+            <Home/>
+          </CheckAuth>
+        ),
+      },
+      {
+        path: '/login',
+        element: (
+          <Guest>
+            <Login />
+          </Guest>
+        ),
+      },
+      {
+        path: '/register',
+        element: (
+          <Guest>
+            <Register />
+          </Guest>
+        ),
+      },
+    ]
+  },
+]);
